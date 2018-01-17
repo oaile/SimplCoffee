bc78145cabd7a8e07e02229a9ba111af8315f500
Oai Le <oailq@fsoft.com.vn>
2018-01-17 19:22:21 +0700

    no message

---
+++ Infrastructure/Data/CoffeeDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class CoffeeDbContext : DbContext
+    {
+        public CoffeeDbContext(DbContextOptions<CoffeeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Post>(ConfigurePost);
+            
+        }
+
+        private void ConfigurePost(EntityTypeBuilder<Post> builder)
+        {
+            //builder.("Catalog");
+        }
+    }
+}
